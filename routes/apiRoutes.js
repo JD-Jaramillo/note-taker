f101ffc0d4852cdd0b84ce9e2293cbe91b577235
John Jaramillo <jaramillo784@gmail.com>
2021-03-15 18:18:16 -0400

    added the routes and now have notes being added to the left column

---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const notesData = require('../db/db.json');
+
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => res.json(notesData));
+
+    app.post('/api/notes', (req, res) => {
+        notesData.push(req.body)
+        res.json(true);
+    })
+};
--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-const notesData = require('./db/db.json')
 const express = require('express');
-const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -9,10 +6,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
-
-app.get('/api/notes', (req, res) => res.json(notesData));
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
